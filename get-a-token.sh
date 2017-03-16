5207cf61c63f72a04675be7da678bf64f80d66e8
Guilhem Tesseyre <guilhem.tesseyre@gmail.com>
2017-03-16 09:45:24 -0700

    Create get-a-token.sh

---
+++ get-a-token.sh
@@ -0,0 +1,5 @@
+REQUEST="{\"auth\": {\"tenantName\":\"$OS_TENANT_NAME\", \"passwordCredentials\": {\"username\": \"$OS_USERNAME\", \"password\": \"$OS_PASSWORD\"}}}"
+RAW_TOKEN=`curl -s -d "$REQUEST" -H "Content-type: application/json" "http://$KEYSTONE_IP:5000/v2.0/tokens"`
+TOKEN=`echo $RAW_TOKEN | python -c "import sys; import json; tok = json.loads(sys.stdin.read()); print tok['access']['token']['id'];"`
+export TOKEN
+echo $TOKEN
